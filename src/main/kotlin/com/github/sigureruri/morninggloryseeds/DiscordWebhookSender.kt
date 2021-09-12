cbcbf0dbb5fccbc06aa314262a6003f78c5005ee
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2021-09-12 22:34:20 +0900

    改行を含むメッセージが送信されない不具合を修正 fix #5

--- src/main/kotlin/com/github/sigureruri/morninggloryseeds/DiscordWebhookSender.kt
+++ src/main/kotlin/com/github/sigureruri/morninggloryseeds/DiscordWebhookSender.kt
@@ -10,7 +10,7 @@ class DiscordWebhookSender(private val webhookUrl: URL) {
 
     fun sendMessage(message: String) {
         val headers = mapOf("User-Agent" to "MorningGlorySeeds")
-        val json = """{"username":"MorningGlorySeeds","content":"$message"}"""
+        val json = """{"username":"MorningGlorySeeds","content":"$message"}""".replace("\n", "\\n")
 
         try {
             httpClient.postWithJson(webhookUrl, headers, json)
